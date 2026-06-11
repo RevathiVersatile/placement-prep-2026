5e4aaebc1db7bb8b79312ac0d7e948680ac4c57a
revathi nilla <rsvdurga7375@gmail.com>
2026-06-11 08:59:14 +0530

    Add MaximumNumberInArray Java program
    Implement a Java program to find the maximum number in an array of five integers.

---
+++ week1/Day2/Block2/MaximumNumberInArray.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class MaximumNumberInArray
+{
+    public static void main(String [] args)
+    {
+        Scanner sc= new Scanner(System.in);
+        int[] arr= new int [5];
+        System.out.println("enter 5 no.s:");
+        
+        for(int i=0;i<5;i++)
+        {
+            arr[i]= sc.nextInt();
+        }
+        int max=arr[0];
+        for(int i=1;i< arr.length;i++)
+        {
+            if(arr[i]>max)
+            {
+                max=arr[i];
+            }
+        }
+System.out.println("maximum number:"+max);
+
+    }
+}
