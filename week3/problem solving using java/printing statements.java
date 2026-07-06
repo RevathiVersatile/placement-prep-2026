9d3a45998a3da98036b2e50795828338444df375
revathi nilla <rsvdurga7375@gmail.com>
2026-07-06 15:57:30 +0530

    Add printing statements Java program
    printing statements-print,println,printf

---
+++ week3/problem solving using java/printing statements.java
@@ -0,0 +1,25 @@
+public class printing
+{
+    public static void main(String[]args)
+    {
+        System.out.println("hello");
+        System.out.print("welcome");
+
+         System.out.println("welcome\b\bhai");
+         System.out.println("hi\b\bhai");
+
+         System.out.println("welcome\thai");
+         System.out.println("welcome\rhai");
+         
+        
+        
+        System.out.printf("%d",10);
+        System.out.printf("%c",'a');
+        System.out.printf("%C",'a');
+        System.out.printf("%s","good");
+        System.out.printf("%S","good");
+        System.out.printf("%f",10.234);
+        System.out.printf("%e",10.234);
+        
+    }
+}
